9fbed454e07bd7aa51285b7558f463587a2856e3
Chris <Stofferdyr@gmail.com>
2019-09-24 14:20:52 +0200

    start singlepage application assignment

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ var names = ["Mert", "Warløkke", "Bhalesh", "Arrax", "Chrillebob"];
 var result = names.filter(name => name.includes("a"));
 
 console.log(result);
-//var x = names.map(name => name.name
-var reverseMap = names.reverse();
-console.log(reverseMap);
+var x = names.map(name => strReverse(name));
+//var reverseMap = names.map(name.reverseString(names));
+console.log(x);
 
-function reverseString(str) {
+function strReverse(str) {
     return str.split("").reverse().join("");
 }
+var all = ["Lars", "Peter", "Jan", "Bo"];
+var y = all.join("*" + " " + ",");
+console.log(y);
+var numbers = [2, 3, 67, 33];
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+console.log(numbers.reduce(reducer));
+
+var thisVariable = {
+    prop: 42,
+    thisFunc: function () {
+        return this.prop + 10;
+    },
+};
+
+console.log(thisVariable.thisFunc());
