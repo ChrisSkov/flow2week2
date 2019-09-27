04059d6c5dc6e01589988e6cb22b30c4f288cb5f
Chris <Stofferdyr@gmail.com>
2019-09-27 10:46:49 +0200

    get joke by id. add joke

--- Onsdag/code_simple_SPA/src/index.js
+++ Onsdag/code_simple_SPA/src/index.js
@@ -4,5 +4,22 @@ import jokes from "./jokes";
 const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
 document.getElementById("jokes").innerHTML = allJokes.join("");
 
+const jokeInput = document.getElementById("jokeInput");
+const outputJoke = document.getElementById("jokeOutput");
+
+jokeInput.oninput = getJokeByID;
+
+function getJokeByID()
+{
+    outputJoke.innerText = jokes.getJokeById(jokeInput.value);
+}
 
 
+const addJokeInput = document.getElementById("addJokeInput");
+document.getElementById("addJokeButton").addEventListener("click", addJokeFromInput);
+
+function addJokeFromInput()
+{
+    let addJoke = addJokeInput.value;
+    jokes.addJoke(addJoke);
+};
